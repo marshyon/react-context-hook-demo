90cb234674e685e7fda27aee4f8ab291d96e5baa
jon brookes <marshyon@gmail.com>
2022-12-15 10:36:45 +0000

    refactor into component files

---
+++ src/components/AddTask.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import { useTasksDispatch } from '../store/TasksContext.js';
+import { v4 as uuidv4 } from 'uuid';
+
+export default function AddTask({ onAddTask }) {
+  
+  const [text, setText] = useState('');
+  const dispatch = useTasksDispatch();
+  
+  const addTask = () => {
+    // console.log('AddTask.addTask()', text);
+    const nextId = uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+    setText('');
+    dispatch({
+      type: 'added',
+      id: nextId,
+      text: text,
+    });
+  };
+
+  return (
+    <>
+      <input
+        placeholder="Add task"
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
+      <button onClick={ () => addTask() }>Add</button>
+    </>
+  );
+}
+
+// let nextId = 3;

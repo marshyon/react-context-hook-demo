bf6ac6eb1f33af36aeca38d1acfc31b81063a5ae
jon brookes <marshyon@gmail.com>
2022-12-15 10:36:17 +0000

    clear out unwanted boilerplate

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import TaskList from './components/TaskList.js';
+import { TasksProvider } from './store/TasksContext.js';
+import AddTask from './components/AddTask.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
+        <TasksProvider>
+          <code>Day off in Kyoto</code>
+          <AddTask />
+          <TaskList />
+        </TasksProvider>
+
+
+
       </header>
     </div>
   );
